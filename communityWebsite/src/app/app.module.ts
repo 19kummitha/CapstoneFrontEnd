6aaccc1d430964ffd2e312704616681837d3e8a9
YogeshwaraRao.Padda <yogeshwararao.padda@ust.com>
2024-09-03 15:19:22 +0530

    add footer component

--- communityWebsite/src/app/app.module.ts
+++ communityWebsite/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AdminloginComponent } from './components/adminlogin/adminlogin.component';
+import { ResidentloginComponent } from './components/residentlogin/residentlogin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    AdminloginComponent,
+    ResidentloginComponent
   ],
   imports: [
     BrowserModule,
---
+++ communityWebsite/src/app/components/residentlogin/residentlogin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-residentlogin',
+  templateUrl: './residentlogin.component.html',
+  styleUrl: './residentlogin.component.css'
+})
+export class ResidentloginComponent {
+
+}
