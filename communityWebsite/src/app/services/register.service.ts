6c3e6e8cddc7b57e327bcbbd13c1dce913586d87
YogeshwaraRao.Padda <yogeshwararao.padda@ust.com>
2024-09-05 17:24:20 +0530

    passing the values in  register form to the end point

--- communityWebsite/src/app/services/register.service.ts
+++ communityWebsite/src/app/services/register.service.ts
@@ -11,6 +11,6 @@ export class RegisterService {
   private apiUrl='http://localhost:5001/api/auth/register-admin';
   constructor(private http:HttpClient) { }
   register(username: string, password: string,email:string,fullname:string,flatnumber:string): Observable<RegisterResponse> {
-    return this.http.post<RegisterResponse>(this.apiUrl, { username, password ,email});
+    return this.http.post<RegisterResponse>(this.apiUrl, { username, password ,email,flatnumber,fullname});
   }
 }
