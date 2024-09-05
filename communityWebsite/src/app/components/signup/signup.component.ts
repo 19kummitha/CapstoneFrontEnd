8cedbf11005592ce12a0fbd6d8d6ec5200fb8104
YogeshwaraRao.Padda <yogeshwararao.padda@ust.com>
2024-09-05 11:30:53 +0530

    add reative form validation for signup form

--- communityWebsite/src/app/components/signup/signup.component.ts
+++ communityWebsite/src/app/components/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RegisterService } from '../../services/register.service';
+import { ValidationService } from '../../services/validation.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,20 +12,40 @@ import { RegisterService } from '../../services/register.service';
 export class SignupComponent {
   username: string = '';
   password: string = '';
-  email:string='';
-  constructor(private router: Router,private loginService:RegisterService) { }
+  email: string = '';
+  constructor(private fb: FormBuilder,private router: Router, private loginService: RegisterService) { }
 
-  onSubmit() {
-    this.router.navigate(['/signup']);
-}
-onLogin() {
-  this.loginService.register(this.username, this.password,this.email).subscribe({
-    next: (response) => {
-      console.log('Register successful', response);
+  registerForm: FormGroup = null!;
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      email:['',[Validators.required,Validators.email]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword:['']
     },
-    error: (error) => {
-      console.error('Registration failed', error);
-    }
-  });
-}
+    { validator: ValidationService.passwordMatchValidator }
+    );
+  }
+  validateControl(input: string) {
+    return ValidationService.validateControl(this.registerForm, input);
+  }
+
+  validateControlError(input: string, errorType: string) {
+    return ValidationService.validateControlError(this.registerForm, input, errorType);
+  }
+
+  onLogin() {
+    this.username=this.registerForm.get('username')?.value;
+    this.password=this.registerForm.get('password')?.value;
+    this.email=this.registerForm.get('email')?.value;
+    this.loginService.register(this.username, this.password, this.email).subscribe({
+      next: (response) => {
+        console.log('Register successful', response);
+      },
+      error: (error) => {
+        console.error('Registration failed', error);
+      }
+    });
+  }
 }
