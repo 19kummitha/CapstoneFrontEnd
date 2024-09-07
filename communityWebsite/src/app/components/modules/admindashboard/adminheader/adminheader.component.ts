b959eff5ef004f6844ffe3af1d19bfe92b092c22
YogeshwaraRao.Padda <yogeshwararao.padda@ust.com>
2024-09-07 09:35:23 +0530

    modify admin header as per requirement

--- communityWebsite/src/app/components/modules/admindashboard/adminheader/adminheader.component.ts
+++ communityWebsite/src/app/components/modules/admindashboard/adminheader/adminheader.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../../../../services/login.service';
 
 @Component({
   selector: 'app-adminheader',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './adminheader.component.css'
 })
 export class AdminheaderComponent {
+  constructor(private router: Router, private loginService: LoginService) {}
+  logout() {
+  
+    // Clear the token from localStorage
+    localStorage.removeItem('token');
+    
+    // Optionally, clear any other user-specific data
+    // this.loginService.clearUserData(); // If you have such a method
+
+    // Redirect to login page
+    this.router.navigate(['/login']);
+  }
 
 }
