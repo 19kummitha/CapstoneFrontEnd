f82d7fc55d215874c02dfc155d7f415b7a5b5941
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-07 06:43:15 +0530

    Added more properties to the registration form.

--- communityWebsite/src/app/components/signup/signup.component.ts
+++ communityWebsite/src/app/components/signup/signup.component.ts
@@ -14,8 +14,9 @@ export class SignupComponent {
   username: string = '';
   passwordHash: string = '';
   email: string = '';
-  flatnumber:string='';
-  fullname:string='';
+  FlatNo:string='';
+  Name:string='';
+  PhoneNumber:string='';
 
   constructor(private fb: FormBuilder,private router: Router, private registerService: RegisterService) { }
 
@@ -24,10 +25,11 @@ export class SignupComponent {
   ngOnInit() {
     this.registerForm = this.fb.group({
       email:['',[Validators.required,Validators.email]],
-      fullname:['',[Validators.required]],
-      flatnumber:['',[Validators.required]],
+      Name:['',[Validators.required]],
+      FlatNo:['',[Validators.required]],
       username: ['', [Validators.required]],
       password: ['', [Validators.required]],
+      PhoneNumber:['',[Validators.required]],
       confirmPassword:['']
     },
     { validator: ValidationService.passwordMatchValidator }
@@ -45,11 +47,14 @@ export class SignupComponent {
     this.username=this.registerForm.get('username')?.value;
     this.passwordHash=this.registerForm.get('password')?.value;
     this.email=this.registerForm.get('email')?.value;
-    this.fullname=this.registerForm.get('fullname')?.value;
-    this.flatnumber = this.registerForm.get('flatnumber')?.value;
-    this.registerService.register(this.username, this.passwordHash, this.email,this.flatnumber,this.fullname).subscribe({
+    this.Name=this.registerForm.get('Name')?.value;
+    this.FlatNo = this.registerForm.get('FlatNo')?.value;
+    this.PhoneNumber=this.registerForm.get('PhoneNumber')?.value;
+
+    this.registerService.register(this.username, this.passwordHash, this.email,this.FlatNo,this.Name,this.PhoneNumber).subscribe({
       next: (response:RegisterResponse) => {
         console.log('Register successful', response);
+        console.log(this.PhoneNumber,this.email,this.FlatNo);
         this.router.navigate(['/login']);
       },
       error: (error) => {
--- communityWebsite/src/app/services/register.service.ts
+++ communityWebsite/src/app/services/register.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LoginResponse } from '../Models/loginresponse';
 import { RegisterResponse } from '../Models/registerresponse';
 
 @Injectable({
@@ -10,7 +9,8 @@ import { RegisterResponse } from '../Models/registerresponse';
 export class RegisterService {
   private apiUrl='http://localhost:5001/api/auth/register';
   constructor(private http:HttpClient) { }
-  register(username: string, passwordHash: string,email:string,fullname:string,flatnumber:string): Observable<RegisterResponse> {
-    return this.http.post<RegisterResponse>(this.apiUrl, { username, passwordHash ,email,flatnumber,fullname});
+  register(username: string, passwordHash: string,email:string,FlatNo:string,Name:string,PhoneNumber:string): Observable<RegisterResponse> {
+    console.log(username,email,Name,passwordHash,PhoneNumber,FlatNo)
+    return this.http.post<RegisterResponse>(this.apiUrl, { username, passwordHash ,email,FlatNo,Name,PhoneNumber});
   }
 }
