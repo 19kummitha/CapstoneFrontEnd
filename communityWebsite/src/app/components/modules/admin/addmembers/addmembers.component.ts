dea2ae332fed0e2d27ad8679d161b4c4667ded4d
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-09 09:56:48 +0530

    functionality of Add Resident is done.

--- communityWebsite/src/app/components/modules/admin/addmembers/addmembers.component.ts
+++ communityWebsite/src/app/components/modules/admin/addmembers/addmembers.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AddResidentDto } from '../../../../Models/addresidentdto';
+import { AddresidentService } from '../../../../services/addresident.service';
 
 @Component({
   selector: 'app-addmembers',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './addmembers.component.css'
 })
 export class AddmembersComponent {
-  resident:R
+  resident:AddResidentDto = {
+    userName: '',
+    email: '',
+    passwordHash: '',
+    flatNo: '',
+    name: '',
+    phoneNumber: ''
+  };
+  successMessage = '';
+  errorMessage = '';
+
+  constructor(private residentService: AddresidentService) {}
+  addResident(): void {
+    this.residentService.addResident(this.resident).subscribe({
+      next: (response) => {
+        this.successMessage = 'Member added successfully!';
+        this.resetForm();
+      },
+      error: (err) => {
+        console.error('Error adding member:', err);
+        this.errorMessage = 'Failed to add member. Please try again.';
+      }
+    });
+}
+resetForm(): void {
+  this.resident = {
+    userName: '',
+    email: '',
+    passwordHash: '',
+    flatNo: '',
+    name: '',
+    phoneNumber: ''
+  };
+}
 }
