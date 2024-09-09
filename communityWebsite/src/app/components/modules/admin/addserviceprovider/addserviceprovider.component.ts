bbdd0c91019cd8a18c3323052134b93dc7993cde
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-09 11:55:34 +0530

    added functionality to add serviceProvider

--- communityWebsite/src/app/components/modules/admin/addserviceprovider/addserviceprovider.component.ts
+++ communityWebsite/src/app/components/modules/admin/addserviceprovider/addserviceprovider.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AddServiceProviderDto } from '../../../../Models/serviceproviderdto';
+import { AddserviceproviderService } from '../../../../services/addserviceprovider.service';
 
 @Component({
   selector: 'app-addserviceprovider',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './addserviceprovider.component.css'
 })
 export class AddserviceproviderComponent {
-
+  serviceprovider:AddServiceProviderDto = {
+    userName: '',
+    email: '',
+    passwordHash: '',
+    serviceType: '',
+    name: '',
+    phoneNumber: ''
+  };
+  successMessage = '';
+  errorMessage = '';
+  constructor(private serviceProviderService: AddserviceproviderService) {}
+  addServiceProvider(): void {
+    this.serviceProviderService.addServiceProvider(this.serviceprovider).subscribe({
+      next: (response) => {
+        this.successMessage = 'Member added successfully!';
+        this.resetForm();
+      },
+      error: (err) => {
+        console.error('Error adding member:', err);
+        this.errorMessage = 'Failed to add member. Please try again.';
+      }
+    });
+}
+resetForm(): void {
+  this.serviceprovider = {
+    userName: '',
+    email: '',
+    passwordHash: '',
+    serviceType: '',
+    name: '',
+    phoneNumber: ''
+  };
+}
 }
