72b9acfbe95144700a7c34878dfbf1304c83be15
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-10 08:16:51 +0530

    Get all posts done.

--- communityWebsite/src/app/components/modules/admin/viewpost/viewpost.component.ts
+++ communityWebsite/src/app/components/modules/admin/viewpost/viewpost.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetPostService } from '../../../../services/getpost.service';
 
 @Component({
   selector: 'app-viewpost',
   templateUrl: './viewpost.component.html',
   styleUrl: './viewpost.component.css'
 })
-export class ViewpostComponent {
+export class ViewpostComponent implements OnInit{
+  posts: any[] = [];
+  errorMessage: string = '';
+  baseUrl: string='http://localhost:5114';
+  constructor(private postService: GetPostService) { }
 
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+  loadPosts() {
+    this.postService.getAllPosts().subscribe(posts => {
+      this.posts = posts.map(post => ({
+        ...post,
+        mediaUrl: this.baseUrl + post.mediaUrl 
+      }));
+    });
+}
 }
