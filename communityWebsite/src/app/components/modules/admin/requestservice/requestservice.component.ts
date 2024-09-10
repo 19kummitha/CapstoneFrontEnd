361a92ce40157371d538156960343b85cdc29440
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-10 08:43:58 +0530

    Request for serviceProvider is done

--- communityWebsite/src/app/components/modules/admin/requestservice/requestservice.component.ts
+++ communityWebsite/src/app/components/modules/admin/requestservice/requestservice.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RequestService } from '../../../../services/request.service';
 
 @Component({
   selector: 'app-requestservice',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './requestservice.component.css'
 })
 export class RequestserviceComponent {
-
+  request = {
+    residentName: '',
+    serviceType: '',
+    serviceDescription: '',
+    flatNo: ''
+  };
+  message: string | null = null;
+  constructor(private requestService:RequestService){}
+  onSubmit(): void {
+    this.requestService.createRequest(this.request).subscribe({
+      next: response => {
+        this.message = 'Service request created successfully';
+        this.resetForm();
+      },
+      error: err => {
+        this.message = 'Failed to create service request';
+        console.error(err);
+      }
+    });
+  }
+  resetForm():void {
+    this.request = {
+      residentName: '',
+      serviceType: '',
+      serviceDescription: '',
+      flatNo: ''
+    };
+  }
 }
---
+++ communityWebsite/src/app/services/request.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+  private baseUrl = 'http://localhost:5114'; // Adjust the base URL as needed
+
+  constructor(private http: HttpClient) { }
+
+  createRequest(request: any): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.post<any>(`${this.baseUrl}/service/request`, request, { headers });
+  }
+}
