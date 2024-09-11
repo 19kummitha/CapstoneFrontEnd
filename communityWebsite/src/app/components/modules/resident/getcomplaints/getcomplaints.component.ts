8cf1595d54c23bf2fe720a178eabc9a7efabd6f9
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-12 05:28:58 +0530

    added functionality to view all the compliants by the resident.

--- communityWebsite/src/app/components/modules/resident/getcomplaints/getcomplaints.component.ts
+++ communityWebsite/src/app/components/modules/resident/getcomplaints/getcomplaints.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Complaint } from '../../../../Models/Complaint';
+import { ResidentcomplaintService } from '../../../../services/residentcomplaint.service';
 
 @Component({
   selector: 'app-getcomplaints',
   templateUrl: './getcomplaints.component.html',
   styleUrl: './getcomplaints.component.css'
 })
-export class GetcomplaintsComponent {
+export class GetcomplaintsComponent implements OnInit {
+  complaints: Complaint[]=[];
 
+  constructor(private complaintService: ResidentcomplaintService) { }
+
+  ngOnInit(): void {
+    this.complaintService.getComplaintsByResident().subscribe((complaints) => {
+      this.complaints = complaints;
+    });
+  }
 }
